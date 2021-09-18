7038f28f4abede6b5a23ba0a8bf227edda6cf164
Maria Jose Loperfido <83318820+MariaJoseLop96@users.noreply.github.com>
2021-09-17 21:26:05 -0300

    Add files via upload

---
+++ desafio9/desafio9.js
@@ -0,0 +1,11 @@
+
+
+let boton2 = document.getElementById("boton2")
+boton2.onclick = () => { alert("ya estas ingresado")}
+boton2.onmouseover = () =>  {console.log("si ya estas registrado")}
+
+const inpText = document.getElementById("inputText")
+  inpText.value
+
+
+ 
