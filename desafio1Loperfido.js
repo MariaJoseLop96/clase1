2b55a6964ac8a2d7f10e36d2b5fe232817d1c135
Maria Jose Loperfido <majoloperfido@hotmail.com>
2021-09-17 21:23:25 -0300

    clase1

---
+++ desafio1Loperfido.js
@@ -0,0 +1,31 @@
+// let nombre = prompt ("INGRESE SU NOMBRE");
+// let apellido =prompt ("Ingrese su apellido");
+// let edad = prompt ("ingrese su edad");
+// let salida = nombre + " " + apellido + " " + "es mi nombre y tengo" + " " +  edad;  
+// alert(salida);
+
+
+// operaciones matematicas 
+
+// suma
+// let numeroUno = 15;
+// let numeroDos = 50;
+// console.log(numeroUno + numeroDos);
+
+// resta
+
+let numero1 = prompt("ingrese el numero");
+let numero2 = prompt("ingrese el segundo numero");
+
+
+resultado = parseInt(numero1) + parseInt (numero2);
+
+console.log = (resultado);
+
+
+
+
+
+
+
+
